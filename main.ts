b96b1ba2ca2c13b579ccc564be5e973d2cc5f9df
Juan9899 <98743364+Juan9899@users.noreply.github.com>
2022-04-06 09:30:50 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,37 +1,48 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 7) {
-        maqueen.motorStop(maqueen.Motors.All)
-    }
-    if (receivedNumber == 8) {
-        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
-            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
-        }
-        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
-        }
-        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
-        }
-        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+    if (receivedNumber == 1) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 120)
+        mcqueen_anda += 1
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else {
+        if (receivedNumber == 2) {
             maqueen.motorStop(maqueen.Motors.All)
+            mcqueen_anda += 0
+            basic.showIcon(IconNames.No)
+        } else {
+            if (receivedNumber == 3) {
+                basic.showLeds(`
+                    . . # . .
+                    . . . # .
+                    # # # . #
+                    . . . # .
+                    . . # . .
+                    `)
+                maqueen.motorStop(maqueen.Motors.All)
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 120)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+                if (mcqueen_anda == 1) {
+                    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 120)
+                } else {
+                    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
+                }
+            }
         }
     }
 })
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-	
-})
+let mcqueen_anda = 0
 radio.setGroup(86)
 let logotipo = 1
 let variable_3 = 0
 let variable_2 = 0
-let mcqueen_anda = 0
+mcqueen_anda = 0
 music.playMelody("E B C5 A B G A F ", 120)
 basic.showString("hola")
-basic.forever(function () {
-	
-})
 basic.forever(function () {
     if (maqueen.Ultrasonic(PingUnit.Centimeters) < 6) {
         maqueen.motorStop(maqueen.Motors.All)
@@ -47,7 +58,11 @@ basic.forever(function () {
 })
 basic.forever(function () {
     maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+    basic.pause(100)
     maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+    basic.pause(100)
     maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+    basic.pause(100)
     maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+    basic.pause(100)
 })
